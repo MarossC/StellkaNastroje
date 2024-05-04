891f4e8d132c07563fc2949b2440035951dc50ee
MarossC <96071677+MarossC@users.noreply.github.com>
2024-05-04 12:05:13 +0200

    Pridani otevirace a preformatovani README

---
+++ Otevirac/otevirac.py
@@ -0,0 +1,146 @@
+import os
+import re
+import time
+import subprocess
+import ctypes
+import sys
+from ctypes import windll
+
+
+try:
+    is_admin = ctypes.windll.shell32.IsUserAnAdmin()
+except:
+    is_admin = False
+
+if not is_admin:
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+    sys.exit()
+
+gameexe = 'start.exe'
+gameconfig = 'settings.cfg'
+
+# check if files exist
+if os.path.isfile(gameexe) and os.path.isfile(gameconfig):
+    print("")
+    pass
+else:
+    print("Script neni ve slozce s Stellarii. Presun do slozky se hrou, a zkus znova.")
+    time.sleep(15)
+    exit()
+
+
+user32 = windll.user32
+s_width = user32.GetSystemMetrics(0)
+s_height = user32.GetSystemMetrics(1)
+
+
+def setWidth(fWidth):
+    with open(gameconfig, 'r+') as f:
+        filedata = f.read()
+        pattern = r"(WIDTH)\s+(\d+)"
+        replacement = r"\1 {}" .format(fWidth)
+        newdata = re.sub(pattern, replacement, filedata)
+        f.seek(0)
+        f.write(newdata)
+        f.close()
+
+def setHeight(fHeight):
+    with open(gameconfig, 'r+') as f:
+        filedata = f.read()
+        pattern = r"(HEIGHT)\s+(\d+)"
+        replacement = r"\1 {}" .format(fHeight)
+        newdata = re.sub(pattern, replacement, filedata)
+        f.seek(0)
+        f.write(newdata)
+        f.close()
+
+
+# - - - - Cele okna
+
+setWidth(9999)
+setHeight(9999)
+
+while True:
+    inputCele = input("Pocet celych oken: ")
+    if inputCele == "":
+        inputCele = 0
+    try:
+        pocetCele = int(inputCele)
+        break
+    except ValueError:
+        print("Zkus to znova, nezadal jsi cislo.\n")
+
+while pocetCele > 0:
+    subprocess.Popen([gameexe])
+    pocetCele = pocetCele - 1
+
+time.sleep(3) # mehehe jdu spat zmrde
+print("\n")
+
+# - - - - Polovicni okno
+
+setWidth(s_width / 2)
+setHeight(s_height - 80)
+
+while True:
+    inputPulka = input("Pocet polovicnich oken: ")
+    if inputPulka == "":
+        inputPulka = 0
+    try:
+        pocetPulka = int(inputPulka)
+        break
+    except ValueError:
+        print("Zkus to znova, nezadal jsi cislo.\n")
+
+while pocetPulka > 0:
+    subprocess.Popen([gameexe])
+    pocetPulka = pocetPulka - 1
+
+time.sleep(3) # mehehe jdu spat zmrde
+print("\n")
+
+# - - - - Normalni Kopacske okno
+
+setHeight((s_height - 110) / 2)
+
+while True:
+    inputMining = input("Pocet normal kopacich oken: ")
+    if inputMining == "":
+        inputMining = 0
+    try:
+        pocetMining = int(inputMining)
+        break
+    except ValueError:
+        print("Zkus to znova, nezadal jsi cislo.\n")
+
+while pocetMining > 0:
+    subprocess.Popen([gameexe])
+    pocetMining = pocetMining - 1
+
+time.sleep(3) # mehehe jdu spat zmrde
+print("\n")
+
+# - - - - Micro Kopacske okno
+
+setWidth(s_width / 4)
+setHeight((s_height - 90) / 2)
+
+while True:
+    inputMicro = input("Pocet mikro kopacich oken: ")
+    if inputMicro == "":
+        inputMicro = 0
+    try:
+        pocetMicro = int(inputMicro)
+        break
+    except ValueError:
+        print("Zkus to znova, nezadal jsi cislo.\n")
+
+while pocetMicro > 0:
+    subprocess.Popen([gameexe])
+    pocetMicro = pocetMicro - 1
+
+print("\n")
+print("####################")
+print("#### By  Maross ####")
+print("####################")
+time.sleep(2)
--- README.md
+++ README.md
@@ -1,18 +1,33 @@
-### Dokumenty / Uzitecne stranky:
+## Dokumenty / Uzitecne stranky:
 
-* **[Dokument na Osvícení; Ingilingibingi](https://docs.google.com/document/d/1afPbFuS__nOqGtFhC083u6rACrxQP9raEN7OBCRNXk4)**
+### **[Dokument na Osvícení; Ingilingibingi](https://docs.google.com/document/d/1afPbFuS__nOqGtFhC083u6rACrxQP9raEN7OBCRNXk4)**
 
-* **[Hledač map; Ingilingibingi](https://osviceni-stellarka.rf.gd)**
+### **[Hledač map; Ingilingibingi](https://osviceni-stellarka.rf.gd)**
 
-* **["Jak nebýt lopata"; Ingilingibingi](https://docs.google.com/spreadsheets/d/1mdnjowAJxLNGDx-habAXJ2iylFDcnLQa1HkoCkDiNwg)**
+### **["Jak nebýt lopata"; Ingilingibingi](https://docs.google.com/spreadsheets/d/1mdnjowAJxLNGDx-habAXJ2iylFDcnLQa1HkoCkDiNwg)**
 
-* **[Přehledné eventy; Sien (WelsyCZ#0198)](https://eventy.g6.cz/)**
+### **[Přehledné eventy; Sien (WelsyCZ#0198)](https://eventy.g6.cz/)**
 
-* **[Wiki; Officiální](https://wiki.stellaria.me/)**
+### **[Wiki; Officiální](https://wiki.stellaria.me/)**
 
-* **[Discord; Officiální](https://discord.gg/invite/MFqTuKvTkv)**
+### **[Discord; Officiální](https://discord.gg/invite/MFqTuKvTkv)**
 
-* **[Aukční Discord; Aukcionáři](https://discord.gg/Kv2hFrTwCs)**
+### **[Aukční Discord; Aukcionáři](https://discord.gg/Kv2hFrTwCs)**
+---
+## Nastoje:
+
+### **Otevirac; Maross**
+##### 1. Stahnout [zde](Otevirac/otevirac.py). (pomoci tohoto tlacitka v pravo nahore: <img width="83" alt="Stahnout" src="https://github.com/MarossC/StellkaNastroje/assets/96071677/13c23b80-fe23-4854-878d-b13e84856cde">)
+
+##### 2. Stahnout a nainstalovat [Python](https://www.python.org/ftp/python/3.12.3/python-3.12.3-amd64.exe).
+
+##### 3. Pretahnout otevirac.py do slozky s hrou, popripade si udelat zastupce.
+
+##### Pouziti vypada takto:
+https://github.com/MarossC/StellkaNastroje/assets/96071677/989b46ef-136a-4a4b-aac9-eac74ede65c1
+
+
+##### Zadat pouze cisla, nebo jen zmacknout enter.
 
 ---
 Pokud zde neco chybi, staci napsat ve hre PMko na "Maross"
